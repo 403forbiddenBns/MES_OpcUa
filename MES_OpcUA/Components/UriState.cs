3cef25a7cb98f12b45d9220d7980bc5944bf02ba
403forbiddenBns <tweek693@gmail.com>
2022-05-15 23:00:14 +0700

    and another

---
+++ MES_OpcUA/Components/UriState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_OpcUA.Components
+{
+    public enum UriState
+    {
+        OK = 0,
+        NotInitialized = 1,
+        BadAddress = 2,
+        NoConnection = 3
+    }
+}
--- MES_OpcUA/ViewModel/MainViewModel.cs
+++ MES_OpcUA/ViewModel/MainViewModel.cs
@@ -1,13 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MES_OpcUA.Model;
+using Opc.UaFx.Client;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MES_OpcUA.ViewModel
 {
     public class MainViewModel : ViewModel
     {
-        // opc.tcp://DESKTOP-4LC5DK7:53530 || 
+        private MainModel mainModel;
+
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public OpcClient OpcClient
+        {
+            get { return opcClient; }
+            set { opcClient = value; }
+        }
+
+        public MainViewModel()
+        {
+            switch (ValidateURI())
+            {
+                case true:
+                    mainModel = new MainModel(Address);
+                    //todo: SOMEHOW SWITCH CONTEXT TO BROWSER WHEN INIT
+                    //todo: INIT BROWSER
+                    break;
+                case false:
+                    var lb = new Label();
+                    MessageBox.Show("Wrong address!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+            }
+            Environment.Exit(0);
+        }
+
+        public bool ValidateURI()
+        {
+           return ((Address.Contains("opc://") && Address.Contains(':')));
+        }
     }
 }
