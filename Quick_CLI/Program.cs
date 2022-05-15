3556bc96bbc0fbd051807d8aeb9342d85d390547
403forbiddenBns <tweek693@gmail.com>
2022-05-15 19:08:10 +0700

    why they always ask me for comment?

---
+++ Quick_CLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+using System;
+
+
+namespace Quick_CLI
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new OpcClient("opc.tcp://DESKTOP-4LC5DK7:53530");
+            client.Connect();
+            /////////////////
+
+            var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+            Browse(node);
+
+
+            //////////////////
+            client.Disconnect();
+            System.Console.WriteLine();
+        }
+
+        static void Browse(OpcNodeInfo node, int level = 0)
+        {
+            string pattern = new string('.', level * 4);
+            var value = node.Attribute(OpcAttribute.DisplayName).Value;
+
+            if (level == 0)
+                Console.WriteLine($"_{level}{pattern}{value}{node.NodeId}");
+
+            level++;
+
+            foreach (var childNode in node.Children())
+                Browse(childNode, level);
+        }
+
+    }
+}
