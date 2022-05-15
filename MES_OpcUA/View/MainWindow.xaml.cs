abd63d1708630f280609db2ec7171c131d55f790
403forbiddenBns <tweek693@gmail.com>
2022-05-15 14:33:08 +0700

    HERE WE GO AGAIN BITCHES

---
+++ MES_OpcUA/View/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MES_OpcUA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            //todo: TO DELETE!
+            //ServerBrowser browser = new ServerBrowser();
+            //browser.Show();
+            //--------
+
+            //this.Close();
+            InitializeComponent();
+        }
+
+    }
+}
