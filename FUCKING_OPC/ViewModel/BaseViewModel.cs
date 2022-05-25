70f2ad579d0a1f46777a1ad1b9f0671d5caf3465
403forbiddenBns <tweek693@gmail.com>
2022-05-25 09:33:50 +0700

    lllllllllllll

--- FUCKING_OPC/ViewModel/BaseViewModel.cs
+++ FUCKING_OPC/ViewModel/BaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace MES_OpcUa.ViewModel
@@ -19,5 +21,35 @@ namespace MES_OpcUa.ViewModel
             OnPropertyChanged(PropertyName);
             return true;
         }
+
+        public static string GetPropertyName<T>(Expression<Func<T>> e)
+        {
+            var member = (MemberExpression)e.Body;
+            return member.Member.Name;
+        }
+
+        /// <summary>
+        /// Raise when property value propertychanged or override propertychage
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyExpression"></param>
+        protected virtual void RaisePropertyChanged<T>
+            (Expression<Func<T>> propertyExpression)
+        {
+            RaisePropertyChanged(GetPropertyName(propertyExpression));
+        }
+        /// <summary>
+        /// Raise when property value propertychanged
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler temp = PropertyChanged;
+            if (temp != null)
+            {
+                temp(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
