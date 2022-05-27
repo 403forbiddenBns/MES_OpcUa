4106dd6c9e56efc7410a792d6b312ff765c6089b
403forbiddenBns <tweek693@gmail.com>
2022-05-27 18:05:09 +0700

    I forgot to save previous code. Time to learn damn git..

--- FUCKING_OPC/Components/INavTreeOpcItem.cs
+++ FUCKING_OPC/Components/INavTreeOpcItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,13 @@ using System.Windows.Media.Imaging;
 
 namespace MES_OpcUa.Components
 {
-    public interface INavTreeOpcItem : INotifyPropertyChanger
+    public interface INavTreeOpcItem : INotifyPropertyChanged
     {
         string FriendlyName { get; set; }
         BitmapSource MyIcon { get; set; }
         string NodeName { get; set; }
 
         ObservableCollection<INavTreeOpcItem> Children { get; }
-        
 
         bool IsExpanded { get; set; }
         bool IncludeOpcVariable { get; set; }
